8f8ef21be4bf1a8efb3d132f57e22d23d52e2585
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 03:04:59 +0600

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int sum = a + b;
+            int subtraction = a - b;
+            float division = (float) a / b; 
+            int multiplication = a * b;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(subtraction);
+            Console.WriteLine(division);
+            Console.WriteLine(multiplication);
+
+        }
+    }
+}
